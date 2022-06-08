f07da89a6ad7110216d39f0ca4500fe5aeb3adc3
iamvs-2002 <iamvs2002@gmail.com>
2022-06-08 15:32:21 +0530

    python-kasa module added

--- app/src/main/python/hello.py
+++ app/src/main/python/hello.py
@@ -1,2 +1,16 @@
-def main() :
-    return "Hey name"
+import asyncio
+from kasa import SmartPlug
+
+async def main():
+    p = SmartPlug("127.0.0.1")
+
+    await p.update()  # Request the update
+    x = (p.alias)  # Print out the alias
+    y = (p.emeter_realtime)  # Print out current emeter status
+
+    await p.turn_off()  # Turn the device off
+
+    return x+y
+
+if __name__ == "__main__":
+    asyncio.run(main())
